a406d4f924b19370845768a3f039df1043782102
GluckGergo <104010789+GluckGergo@users.noreply.github.com>
2025-11-28 15:27:22 +0100

    Code added to treatment controller

--- BeautySalon.Endpoint/Controllers/TreatmentController.cs
+++ BeautySalon.Endpoint/Controllers/TreatmentController.cs
@@ -1,7 +1,42 @@
-﻿namespace BeautySalon.Endpoint.Controllers
+﻿using BeautySalon.Entities.Dto.Treatment;
+using BeautySalon.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautySalon.Endpoint.Controllers
 {
+    [ApiController]
+    [Route("{controller}")]
     public class TreatmentController
     {
+        TreatmentLogic logic;
 
+        public TreatmentController(TreatmentLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<TreatmentViewDto> Get()
+        {
+            return logic.Read();
+        }
+
+        [HttpPost]
+        public async Task Post(TreatmentCreateUpdateDto dto)
+        {
+            await logic.Create(dto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(string id)
+        {
+            await logic.Delete(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task Update(string id, [FromBody] TreatmentCreateUpdateDto dto)
+        {
+            await logic.Update(id, dto);
+        }
     }
 }
