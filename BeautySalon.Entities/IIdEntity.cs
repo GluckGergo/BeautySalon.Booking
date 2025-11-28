afae8b1a63551d4197ef60e93bd72ff286bd296b
GluckGergo <104010789+GluckGergo@users.noreply.github.com>
2025-11-28 10:23:53 +0100

    IIdEntity added

---
+++ BeautySalon.Entities/IIdEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySalon.Entities
+{
+    public interface IIdEntity
+    {
+        public string Id { get; set; }
+    }
+}
