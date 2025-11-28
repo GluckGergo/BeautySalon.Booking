b4235d6574c97b00b122cb51944a6642de088a07
GluckGergo <104010789+GluckGergo@users.noreply.github.com>
2025-11-28 10:04:33 +0100

    Properties added to entities

--- BeautySalon.Entities/Appointment.cs
+++ BeautySalon.Entities/Appointment.cs
@@ -1,6 +1,16 @@
-﻿namespace BeautySalon.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BeautySalon.Entities
 {
     public class Appointment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public DateTime Date { get; set; }
+        public string TreatmentId { get; set; } = string.Empty;
+        public Treatment? Treatment { get; set; }
     }
 }
--- BeautySalon.Entities/Treatment.cs
+++ BeautySalon.Entities/Treatment.cs
@@ -1,6 +1,17 @@
-﻿namespace BeautySalon.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BeautySalon.Entities
 {
     public class Treatment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [StringLength(250)]
+        public string Name { get; set; } = string.Empty;
+        public int Time { get; set; }
+        public ICollection<Appointment>? Appointments { get; set; }
     }
 }
